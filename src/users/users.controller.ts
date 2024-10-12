0111966acbd049a7f2a37c5cb774d8dfe07aa362
Kehinde <onifkay@gmail.com>
2024-10-12 03:10:14 +0100

    feat: controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(
+    @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN',
+  ): string | any[] {
+    return [
+      {
+        id: 1,
+        name: 'John Doe',
+        role: 'INTERN',
+      },
+      {
+        id: 2,
+        name: 'Jane Doe',
+        role: 'ENGINEER',
+      },
+    ];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
